d2bdc52be57751c52335c567fa061f3374201071
Marc Delorme <marc@marcdelorme.fr>
2012-08-10 18:31:49 -0700

    fix cleaner daemon

--- app/worker.js
+++ app/worker.js
@@ -2,9 +2,10 @@ var clean = require('./controllers/clean'),
 	conf  = require('../config/config');
 
 var cleaner = function(period) {
-	clean.cleanPlayer(conf.get('player:timeout'));
-	clean.cleanGroup();
-
-	setTimeout(cleaner, period * 1000);
+	setTimeout(function() {
+		clean.cleanPlayer(conf.get('player:timeout'));
+		clean.cleanGroup();
+		cleaner(period);
+	}, period * 1000);
 };
 exports.cleaner = cleaner;
